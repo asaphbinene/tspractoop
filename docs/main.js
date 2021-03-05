2683af14ba7266388e41b77dd17f0911a6c6731a
KAT Binene <asaphbinene@outlook.com>
2021-03-05 18:54:53 +0200

    Committing a standard class of ts

--- docs/main.js
+++ docs/main.js
@@ -1,14 +1,32 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Department = /** @class */ (function () {
     /**
-     *
+     * constractor declaration
      */
-    function Department(n) {
+    function Department(id, n) {
+        this.n = n;
         this.employees = [];
         this.name = n;
+        this.id = id;
     }
+    //List of tdefined and declared method 
     Department.prototype.describe = function () {
-        console.log("Department: " + this.name);
+        console.log("Department: (" + this.id + "): " + this.name);
     };
     Department.prototype.addEmployee = function (employee) {
         this.employees.push(employee);
@@ -19,10 +37,54 @@ var Department = /** @class */ (function () {
     };
     return Department;
 }());
-var accounting = new Department('Accounting');
+var ITDepartment = /** @class */ (function (_super) {
+    __extends(ITDepartment, _super);
+    /**
+     *
+     */
+    function ITDepartment(id, admins) {
+        var _this = _super.call(this, id, 'IT') || this;
+        _this.admins = admins;
+        return _this;
+    }
+    return ITDepartment;
+}(Department));
+var AccountingDepartment = /** @class */ (function (_super) {
+    __extends(AccountingDepartment, _super);
+    /**
+     *
+     */
+    function AccountingDepartment(id, reports) {
+        var _this = _super.call(this, id, 'Accounting') || this;
+        _this.reports = reports;
+        _this.lastReport = reports[0];
+        return _this;
+    }
+    AccountingDepartment.prototype.addEmployee = function (name) {
+        if (name === 'Asaph') {
+            return;
+        }
+        this.employees.push(name);
+    };
+    AccountingDepartment.prototype.addReport = function (text) {
+        this.reports.push(text);
+        this.lastReport = 'text';
+    };
+    AccountingDepartment.prototype.printReports = function () {
+        console.log(this.reports);
+    };
+    return AccountingDepartment;
+}(Department));
+var accounting = new Department('D1', 'Accounting');
+var mydepartment = new AccountingDepartment('D2', []);
+var allDepartment = new ITDepartment('D0', []);
 accounting.addEmployee("Asaph");
 accounting.addEmployee("Mado Binene");
-accounting.employees[2] = "Gabriel";
 console.log(accounting);
 accounting.describe();
 accounting.printEmployeeInfo();
+mydepartment.addEmployee("Asaph");
+mydepartment.addEmployee('Mado');
+mydepartment.addReport('From the last test there something to be monitor');
+mydepartment.printReports();
+mydepartment.printEmployeeInfo();
--- src/app/main.ts
+++ src/app/main.ts
@@ -1,16 +1,21 @@
 class Department {
-    name : string;
-    employees : string[] = [];
+
+    //Class properties declaration
+    private id: string;
+    public name : string;
+    protected employees : string[] = [];
 
     /**
-     *
+     * constractor declaration 
      */
-    constructor(n: string) {
+    constructor( id: string, public n: string, ) {
         this.name = n;
+        this.id = id;
     }
 
+    //List of tdefined and declared method 
     describe( this: Department ) {
-        console.log("Department: " + this.name);
+        console.log(`Department: (${this.id}): ${this.name}`);
     }
 
     addEmployee(employee: string) {
@@ -23,16 +28,63 @@ class Department {
     }
 }
 
-const accounting = new Department('Accounting');
+class ITDepartment extends Department {
+    admins: string[];
+    /**
+     *
+     */
+    constructor(id: string, admins: string[]) {
+        super(id, 'IT');
+        this.admins = admins;
+    }
+}
+
+class AccountingDepartment extends Department{
+    private lastReport: string;
+    /**
+     *
+     */
+    constructor(id: string, private reports: string[]) {
+        super(id, 'Accounting');
+        this.lastReport = reports[0];
+    }
+
+    addEmployee(name: string){
+        if(name === 'Asaph'){
+            return;
+        }
+
+        this.employees.push(name);
+    }
+
+    addReport(text: string) {
+        this.reports.push(text);
+        this.lastReport = 'text';
+    }
+
+    printReports(){
+        console.log(this.reports);
+    }
+}
+
+const accounting = new Department('D1', 'Accounting');
+
+const mydepartment = new AccountingDepartment('D2', []);
+
+const allDepartment = new ITDepartment('D0', [])
 
 
 accounting.addEmployee("Asaph");
 accounting.addEmployee("Mado Binene");
-
-accounting.employees[2] = "Gabriel";
 
 console.log(accounting);
 accounting.describe();
 accounting.printEmployeeInfo()
 
 
+mydepartment.addEmployee("Asaph");
+mydepartment.addEmployee('Mado');
+
+mydepartment.addReport('From the last test there something to be monitor');
+mydepartment.printReports();
+mydepartment.printEmployeeInfo();
